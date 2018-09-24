ac4a0f5308b443bc1e46df99f8dccff9cb827aac
NikolasUntoten <nikolasr@ggaub.com>
2018-09-23 21:26:22 -0700

    added firebase to project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+
+var config = {
+	apiKey: 'AIzaSyDhrQdnHqEI3LVG2veiT0GNAnVEz0NJslY'
+	authDomain: 'flow-chat-800fc.firebaseapp.com'
+	databaseURL: 'https://flow-chat-800fc.firebaseio.com/'
+	storageBucket: 'gs://flow-chat-800fc.appspot.com'
+};
+firebase.initializeApp(config);
+var storage = firebase.storage();
 
 class App extends Component {
   render() {
